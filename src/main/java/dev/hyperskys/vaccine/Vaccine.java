374b9dbfbdb2eca5384dd329ea6ca2a64d994408
HyperSys <66301237+HyperSkys@users.noreply.github.com>
2022-06-28 23:14:18 -0600

    Useless Update

--- src/main/java/dev/hyperskys/vaccine/Vaccine.java
+++ src/main/java/dev/hyperskys/vaccine/Vaccine.java
@@ -2,14 +2,11 @@ package dev.hyperskys.vaccine;
 
 public class Vaccine {
 
-    public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_BLACK = "\u001B[30m";
     public static final String ANSI_RED = "\u001B[31m";
     public static final String ANSI_GREEN = "\u001B[32m";
     public static final String ANSI_YELLOW = "\u001B[33m";
     public static final String ANSI_BLUE = "\u001B[34m";
     public static final String ANSI_PURPLE = "\u001B[35m";
-    public static final String ANSI_CYAN = "\u001B[36m";
     public static final String ANSI_WHITE = "\u001B[37m";
 
     public static void main(String[] args) throws InterruptedException {
@@ -31,14 +28,10 @@ public class Vaccine {
         System.exit(100);
     }
 
-    public static void writeScreen(String text) {
+    public static void writeScreen(String text) throws InterruptedException {
         for(int i = 0; i < text.length(); i++) {
             System.out.printf("%c", text.charAt(i));
-            try{
-                Thread.sleep(50);
-            }catch(InterruptedException ex){
-                Thread.currentThread().interrupt();
-            }
+            Thread.sleep(50);
         }
     }
 }
